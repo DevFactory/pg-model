ed5c60ba71ef5709aa6758d9f089f270f2e2276b
Shankar Vasudevan <shankar@polyglotted.io>
2016-01-03 15:56:04 +0000

    remodel hidden fields - fixes #29, fixes #32

--- src/main/java/io/polyglotted/pgmodel/search/index/HiddenFields.java
+++ src/main/java/io/polyglotted/pgmodel/search/index/HiddenFields.java
@@ -1,18 +1,20 @@
 package io.polyglotted.pgmodel.search.index;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Maps;
+import com.google.common.collect.ImmutableMap;
+import io.polyglotted.pgmodel.search.DocStatus;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static io.polyglotted.pgmodel.search.index.FieldMapping.notAnalyzedStringField;
 import static io.polyglotted.pgmodel.search.index.FieldMapping.simpleField;
 
 public abstract class HiddenFields {
     public static final String ANCESTOR_FIELD = "&ancestor";
+    public static final String APPROVAL_ROLES_FIELD = "&approvalRoles";
     public static final String BASEKEY_FIELD = "&baseKey";
     public static final String BASEVERSION_FIELD = "&baseVersion";
     public static final String BYTES_FIELD = "&bytes";
@@ -21,11 +23,8 @@ public abstract class HiddenFields {
     public static final String STATUS_FIELD = "&status";
     public static final String TIMESTAMP_FIELD = "&timestamp";
     public static final String UNIQUE_FIELD = "&uniqueId";
-    public static final String USER_FIELD = "&user";
     public static final String UPDATER_FIELD = "&updater";
-
-    private static final Set<String> HEADER_FIELDS = ImmutableSet.of(ANCESTOR_FIELD, BASEKEY_FIELD, BASEVERSION_FIELD,
-       COMMENT_FIELD, EXPIRY_FIELD, STATUS_FIELD, TIMESTAMP_FIELD, UNIQUE_FIELD, USER_FIELD, UPDATER_FIELD);
+    public static final String USER_FIELD = "&user";
 
     public static List<FieldMapping> hiddenFields() {
         return ImmutableList.of(
@@ -39,7 +38,36 @@ public abstract class HiddenFields {
            notAnalyzedStringField(USER_FIELD).docValues(true).includeInAll(false).build());
     }
 
-    public static Map<String, Object> headerMapFrom(Map<String, Object> source) {
-        return Maps.filterKeys(source, HEADER_FIELDS::contains);
+    @VisibleForTesting
+    public static ImmutableMap<String, Object> headerFrom(Map<String, ?> map) {
+        ImmutableMap.Builder<String, Object> builder = ImmutableMap.builder();
+        putVal(map, APPROVAL_ROLES_FIELD, builder);
+        putVal(map, reqdProp(map, BASEKEY_FIELD), builder);
+        putLongStr(map, BASEVERSION_FIELD, builder);
+        putVal(map, COMMENT_FIELD, builder);
+        putLongStr(map, EXPIRY_FIELD, builder);
+        putStatus(map, STATUS_FIELD, builder);
+        putLongStr(map, reqdProp(map, TIMESTAMP_FIELD), builder);
+        putVal(map, UNIQUE_FIELD, builder);
+        putVal(map, UPDATER_FIELD, builder);
+        putVal(map, reqdProp(map, USER_FIELD), builder);
+        return builder.build();
+    }
+
+    private static void putStatus(Map<String, ?> map, String property, ImmutableMap.Builder<String, Object> builder) {
+        if (map.containsKey(property)) builder.put(property, DocStatus.fromStatus((String) map.get(property)));
+    }
+
+    private static void putLongStr(Map<String, ?> map, String property, ImmutableMap.Builder<String, Object> builder) {
+        if (map.containsKey(property)) builder.put(property, Long.parseLong((String) map.get(property)));
+    }
+
+    private static void putVal(Map<String, ?> map, String property, ImmutableMap.Builder<String, Object> builder) {
+        if (map.containsKey(property)) builder.put(property, map.get(property));
+    }
+
+    private static String reqdProp(Map<String, ?> map, String property) {
+        checkArgument(map.containsKey(property), property + " is missing in the map");
+        return property;
     }
 }
--- src/test/java/io/polyglotted/pgmodel/search/index/HiddenFieldsTest.java
+++ src/test/java/io/polyglotted/pgmodel/search/index/HiddenFieldsTest.java
@@ -1,6 +1,7 @@
 package io.polyglotted.pgmodel.search.index;
 
 import com.google.common.collect.ImmutableMap;
+import io.polyglotted.pgmodel.search.DocStatus;
 import org.testng.annotations.Test;
 
 import java.util.Map;
@@ -20,11 +21,14 @@ public class HiddenFieldsTest extends HiddenFields {
 
     @Test
     public void headerMapFromHiddenFields() {
-        Map<String, Object> expected = ImmutableMap.<String, Object>of(STATUS_FIELD, "expired",
-           COMMENT_FIELD, "my comment");
-        Map<String, Object> input = ImmutableMap.<String, Object>of("bar", true, STATUS_FIELD, "expired",
-           "baz", 25, COMMENT_FIELD, "my comment", BYTES_FIELD, "foobarbazqux");
+        Map<String, Object> expected = ImmutableMap.<String, Object>of(STATUS_FIELD, DocStatus.EXPIRED,
+           COMMENT_FIELD, "my comment", BASEKEY_FIELD, "base", TIMESTAMP_FIELD, 25L, USER_FIELD, "user");
 
-        assertThat(headerMapFrom(input), is(expected));
+        Map<String, Object> input = ImmutableMap.<String, Object>builder()
+           .put(BASEKEY_FIELD, "base").put("bar", true).put(STATUS_FIELD, "expired").put("baz", 25)
+           .put(COMMENT_FIELD, "my comment").put(BYTES_FIELD, "foobarbazqux").put(TIMESTAMP_FIELD, "25")
+           .put(USER_FIELD, "user").build();
+
+        assertThat(headerFrom(input), is(expected));
     }
 }
